29eeac34fd98cf80c4e72633c7b4f5b5c495d6ad
Kristinimum <kristinmaxwell43@gmail.com>
2024-01-05 16:03:37 +0000

    frame rate

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,9 +8,10 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()  # this function initializes background settings for pygame.
+        self.clock = pygame.time.Clock()
         
         self.screen = pygame.display.set_mode((1200, 800)) # creates display window assigned to self.screen
-        pygame.display.set_caption("Alien Invasion")  
+        pygame.display.set_caption("Alien Invasion")
         
     def run_game(self):
         """Start the main loop for the game.""" # run_game is a method that controls the game.
@@ -22,6 +23,7 @@ class AlienInvasion:
                     
                 # Make the most recently drawn screen visible.
                 pygame.display.flip()
+                self.clock.tick(60)
                 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
