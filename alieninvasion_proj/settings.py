f00d22493d7ede00942e5797a8a2a4c069b11ef6
Kristin Maxwell <kristinmaxwell43@gmail.com>
2024-01-06 20:25:43 -0700

    ship speed

--- alieninvasion_proj/settings.py
+++ alieninvasion_proj/settings.py
@@ -7,4 +7,7 @@ class Settings:
         self.screen_width = 600
         self.screen_height = 600
         self.bg_color = (255,182,193)
+
+        # Ship settings
+        self.ship_speed = 1.5
         
