f68b0a0a1f8b179a28d268cb05eb7fac43bd9c67
Kristin Maxwell <kristinmaxwell43@gmail.com>
2024-01-12 09:52:27 -0700

    bool

--- python_fundamentals/data_types.py
+++ python_fundamentals/data_types.py
@@ -2,23 +2,22 @@
 """
 
 def main():
-# String (str)
+    # String (str)
     instance_type = "t2.micro"
     message = "My instances are of type: "
 
-
-
-# Integar (int)
+    # Integar (int)
     num_of_instances = 5
     hours_running = 10
 
     print(f"{message} {instance_type}. I have {num_of_instances} \nof them and they have been running {hours_running} hours.")
 
-# Boolean (bool)
+    # Boolean (bool)
+    instances_running = True
+    print(f"Are my instances running? {instances_running}.")
+    print(f"My variable is of type: {type(instances_running)}.")
 
-
-
-# Floating point number (float)
+    # Floating point number (float)
 
 
 
