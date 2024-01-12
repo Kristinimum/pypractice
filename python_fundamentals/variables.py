55e4143b7a83c61b0615dc7256fc2438fa8e2b7f
Kristin Maxwell <kristinmaxwell43@gmail.com>
2024-01-12 09:31:25 -0700

    variables

---
+++ python_fundamentals/variables.py
@@ -0,0 +1,11 @@
+# Create first string variable
+name = "Mike"
+profession = "writer"
+
+# Create variable containing integar
+books_written = 6
+
+#Concatenate and print out variables
+output = name + " is a " + profession + " and he's written " + str(books_written) + " books. "
+
+print(output)
