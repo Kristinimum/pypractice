01547079cc87d02377101259c62c498eb055fba9
Kristin Maxwell <kristinmaxwell43@gmail.com>
2024-01-12 09:33:53 -0700

    outputs

--- python_fundamentals/variables.py
+++ python_fundamentals/variables.py
@@ -7,5 +7,6 @@ books_written = 6
 
 #Concatenate and print out variables
 output = name + " is a " + profession + " and he's written " + str(books_written) + " books. "
-
-print(output)
+# This option below is the main way we will write our outputs.
+output_2 = f"{name} is a {profession} and he's written {books_written} books."
+print(output_2)
