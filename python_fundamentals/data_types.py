22b57cef290726a64bafbebe7d630860c9522c40
Kristin Maxwell <kristinmaxwell43@gmail.com>
2024-01-12 09:48:17 -0700

    beginning data types

---
+++ python_fundamentals/data_types.py
@@ -0,0 +1,26 @@
+"""Create variables of different data types and display times.
+"""
+
+def main():
+# String (str)
+    instance_type = "t2.micro"
+    message = "My instances are of type: "
+
+
+
+# Integar (int)
+    num_of_instances = 5
+    hours_running = 10
+
+    print(f"{message} {instance_type}. I have {num_of_instances} \nof them and they have been running {hours_running} hours.")
+
+# Boolean (bool)
+
+
+
+# Floating point number (float)
+
+
+
+if __name__ == "__main__":
+    main()
