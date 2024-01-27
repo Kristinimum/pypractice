0680c470a59d24d1d7d39d457c1560b0579fc789
Kristin Maxwell <kristinmaxwell43@gmail.com>
2024-01-27 09:46:51 -0700

    prevented more instances being created each time command is run

--- python_fundamentals/aws_services_py/ec2_hol.py
+++ python_fundamentals/aws_services_py/ec2_hol.py
@@ -14,41 +14,41 @@ instance_id = None
 # and only work with an instance that has not been terminated. create a list called instances.
 #before this code we ran line 30's code to make an instance. so we don't keep creating new instances
 # we then used this for loop to iterate through the list of instances to only work with the instance we created. 
-#instances = ec2.instances.all()
-#instance_exists = False
+instances = ec2.instances.all()
+instance_exists = False
+#each instance doing a nested for loop inside and if finds our tagged instance will change the bool to true so doesnt get created again
+for instance in instances:
+    for tag in instance.tags:
+        if tag['Key'] == 'Name' and tag['Value'] == instance_name:
+            instance_exists = True
+            instance_id = instance.id
+            print(f"An instance named '{instance_name}' with id '{instance_id}' already exists.")
+            break
+    if instance_exists:
+        break
 
-#for instance in instances:
- #   for tag in instance.tags:
-  #      if tag['Key'] == 'Name' and tag['Value'] == instance_name:
-   #         instance_exists = True
-    #        instance_id = instance.id
-     #       print(f"An instance named '{instance_name}' with id '{instance_id}' already exists.")
-      #      break
-    #if instance_exists:
-     #   break
-
-#if not instance_exists:   # then we indented everything else below by 4 spaces.
-#Launch a new ec2 Instance if it hasn't already been created
-new_instance = ec2.create_instances(
-        ImageId='ami-0a3c3a20c09d6f377',  #i did this replace with a valid AMI ID
-        MinCount=1,
-        MaxCount=1,
-        InstanceType='t2.micro',
-        KeyName='jenkins-kp',  # replace with your key pair name
-        TagSpecifications=[
-            {
-                'ResourceType': 'instance',
-                'Tags': [
-                    {
-                        'Key': 'Name',
-                        'Value': instance_name
-                    },
-                ]
-            },
-        ]
-    )
-instance_id = new_instance[0].id
-print(f"Instance named '{instance_name}' with id '{instance_id}' created.")
+if not instance_exists:   # then we indented everything else below by 4 spaces.
+    #Launch a new ec2 Instance if it hasn't already been created
+    new_instance = ec2.create_instances(
+            ImageId='ami-0a3c3a20c09d6f377',  #i did this replace with a valid AMI ID
+            MinCount=1,
+            MaxCount=1,
+            InstanceType='t2.micro',
+            KeyName='jenkins-kp',  # replace with your key pair name
+            TagSpecifications=[
+                {
+                    'ResourceType': 'instance',
+                    'Tags': [
+                        {
+                            'Key': 'Name',
+                            'Value': instance_name
+                        },
+                    ]
+                },
+            ]
+        )
+    instance_id = new_instance[0].id
+    print(f"Instance named '{instance_name}' with id '{instance_id}' created.")
 # Stop an instance. stop, start, terminate are all functions.
 #ec2.Instance(instance_id).stop()
 #print(f"Instance '{instance_name}--{instance_id}' has been stopped.")
