9a3c5cf024a4c6d3133ffd5a9287ee67868f7234
Kristin Maxwell <kristinmaxwell43@gmail.com>
2024-01-27 11:18:59 -0700

    created and attached IGW

--- python_fundamentals/aws_services_py/vpc_hol.py
+++ python_fundamentals/aws_services_py/vpc_hol.py
@@ -23,3 +23,22 @@ else:
     ec2.create_tags(Resources=[vpc_id], Tags=[{'Key': 'Name', 'Value' : vpc_name}])
     print(f"VPC '{vpc_name}' with ID '{vpc_id}'has been created.")
 
+# Create internet gateway
+ig_name = 'ig-vpc-hol'
+response = ec2.describe_internet_gateways(
+    Filters=[{'Name': 'tag:Name', 'Values': [ig_name]}]
+    )
+internet_gateways = response.get('InternetGateways', [])
+
+if internet_gateways:
+    ig_id = internet_gateways[0]['InternetGatewayId']
+    print(f"Internet Gateway'{ig_name}' with ID '{ig_id}'already exists.")
+else:
+    ig_response = ec2.create_internet_gateway()
+    ig_id = ig_response['InternetGateway']['InternetGatewayId']
+
+    ec2.create_tags(Resources=[ig_id], Tags=[{'Key': 'Name', 'Value' : ig_name}])
+    ec2.attach_internet_gateway(VpcId=vpc_id, InternetGatewayId=ig_id)
+    print(f"Internet Gateway'{ig_name}' with ID '{ig_id}'has been created.")
+
+# Create a route table and a public route to the internet
